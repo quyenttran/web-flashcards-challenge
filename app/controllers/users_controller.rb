8145e595626779f690617dd9f470e4605bf3e73e
Ian Gonnella & Jared Graham & Paul Newsam & Quyen Tran <pair+iagon.jaredgraham12.monkypuzzle.quyenttran@devbootcamp.com>
2017-05-12 15:01:54 -0700

    Adds user login/logout and layout.erb styling

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,53 @@
+# Index - display registration form
+get '/users' do
+  erb :'/users/index'
+end
+
+# New - display form to create new user
+# Not necessary (on index)
+
+# Create - create new user
+post '/users/new' do
+  # Create new user with post data
+  user = User.create(params['user'])
+  # Log in user
+  session['user_id'] = user.id
+  redirect to "/users/#{user.id}"
+end
+
+# Misc: Login(new) - display login form
+get '/users/login' do
+  erb :'/users/login'
+end
+
+# Misc: Login(create) -
+post '/users/login' do
+  # Authenticate login
+  if User.authenticate(params['username'], params['password'])
+    # Log in user
+    user = User.find_by(username: params['username'])
+    session['user_id'] = user.id
+    redirect to '/'
+  else
+    # Bounce back to login
+    redirect to '/users/login'
+  end
+end
+
+# Edit
+# Unnecessary
+
+# Update
+# Unnecessary
+
+# Show - display user profile
+get '/users/:id' do
+  erb :'/users/show'
+end
+
+# Destroy - logout user
+delete '/users/:id' do
+  # Log out user
+  session.delete('user_id')
+  redirect to '/users'
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,22 @@
+require 'bcrypt'
+
 class User < ApplicationRecord
   # Remember to create a migration!
+  include BCrypt
   has_many :games
   validates :username, :hashed_password, {presence: true}
+
+  def password
+    @password ||= Password.new(hashed_password)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.hashed_password = @password
+  end
+
+  def self.authenticate(username, password)
+    @user = User.find_by(username: username)
+    @user.password == password ? true : false
+  end
 end
