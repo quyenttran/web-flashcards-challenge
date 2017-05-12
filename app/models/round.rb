af3107e0a7160682d0e0ab2fa55d87a66ffd4076
Ian Gonnella <iangonnella@gmail.com>
2017-05-12 16:25:14 -0700

    Add file for game end display

--- app/models/round.rb
+++ app/models/round.rb
@@ -2,4 +2,18 @@ class Round < ApplicationRecord
   # Remember to create a migration!
   belongs_to :game
   has_many :guesses
+
+
+  # def correct_guesses
+  #   count = 0
+  #   (self.game.deck.cards.count).times do
+  #     self.guesses.each do |guess|
+  #       if guess.card.id ==  self.deck.card.id
+  #       guess.downcase == self.game.deck.card.answer.downcase ? count+=1 : count+=0
+  #       end
+  #     end
+  #   end
+  #   count
+  # end
+
 end
