be76cdbd4c83701bbff5d00af405e6cbafbfa46a
Josh Lim & Ryan Austin <pair+joshhlim.ryana760@devbootcamp.com>
2017-05-13 14:36:36 -0700

    Finish

--- db/migrate/20170512112020_create_rounds.rb
+++ db/migrate/20170512112020_create_rounds.rb
@@ -2,6 +2,7 @@ class CreateRounds < ActiveRecord::Migration[5.0]
   def change
     create_table :rounds do |t|
       t.integer :score, default: 0
+      t.integer :guess, default: 0
       t.integer :user_id
       t.integer :deck_id
       t.integer :card_id
--- db/migrate/20170512112025_create_guesses.rb
+++ db/migrate/20170512112025_create_guesses.rb
@@ -1,10 +1,8 @@
 class CreateGuesses < ActiveRecord::Migration[5.0]
   def change
     create_table :guesses do |t|
-      t.integer :count, default: 0
       t.string :guess, null: false
       t.integer :user_id
-      t.integer :card_id
 
       t.timestamps(null: false)
     end
