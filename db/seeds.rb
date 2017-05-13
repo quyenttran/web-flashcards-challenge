e5608df305591692066c6934ec16a85b4c93784f
Erik Bonn & Lisa D'Asaro & Mendel Dinh & Paul Vander <pair+erikbonn.lmdasaro.mdinh7.paulsvander@devbootcamp.com>
2017-05-12 21:23:50 -0700

    Profile page correctly lists stats

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 
 Deck.create(name: "raccoon")
+Deck.create(name: "otter")
 
 Card.create(
   question: "What is a nighthawk's primary food source?",
@@ -27,5 +28,63 @@ Card.create(
   deck_id: 1,
 )
 
+Card.create(
+  question: "What is an otter's primary food source?",
+  answer: "fish",
+  deck_id: 2,
+)
+Card.create(
+  question: "True or false? Otters spend the majority of their time on land.",
+  answer: "true",
+  deck_id: 2,
+)
+Card.create(
+  question: "How many species of otters are there?",
+  answer: "13",
+  deck_id: 2,
+)
+Card.create(
+  question: "True of false? Otters are native to Australia.",
+  answer: "false",
+  deck_id: 2,
+)
+Card.create(
+  question: "True or false? Otters make and use tools.",
+  answer: "true",
+  deck_id: 2,
+)
+Card.create(
+  question: "In years, what is the average life span of an otter in the wild?",
+  answer: "10",
+  deck_id: 2,
+)
 
+User.create(
+  username: "aaa",
+  password: "aaa"
+  )
+
+Round.create(
+  user_id: 1,
+  deck_id: 1,
+  complete: 1,
+  first_tries: 4,
+  total_guesses: 10
+  )
+
+Round.create(
+  user_id: 1,
+  deck_id: 1,
+  complete: 1,
+  first_tries: 2,
+  total_guesses: 4
+  )
+
+Round.create(
+  user_id: 1,
+  deck_id: 2,
+  complete: 1,
+  first_tries: 3,
+  total_guesses: 20
+  )
 
