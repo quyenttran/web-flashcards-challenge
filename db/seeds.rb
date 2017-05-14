b6ec65978d4fdaa26bb66d067c887705e9b71cad
monkypuzzle <paul.newsam@gmail.com>
2017-05-14 16:34:17 -0700

    Minor edits

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,16 +3,16 @@ require 'csv'
 
 
 # Create nighthawks flashcard deck
-deck = Deck.create(name: 'Nighthawks')
+nighthawks_deck = Deck.create(name: 'Nighthawks')
 nighthawk = [{ question: "What is a nighthawk's primary food source?",
-answer: "insects", deck_id: deck.id},
+answer: "insects", deck_id: nighthawks_deck.id},
 {question: "True or false?  Nighthawks are closely related to hawks.",
-  answer: "false", deck_id: deck.id},
-{question: "True or false?  Nighthawks make nests.", answer: "false", deck_id: deck.id},
+  answer: "false", deck_id: nighthawks_deck.id},
+{question: "True or false?  Nighthawks make nests.", answer: "false", deck_id: nighthawks_deck.id},
 {question: "Where do Common Nighthawks spend the winter?
-", answer: "South America", deck_id: deck.id},
+", answer: "South America", deck_id: nighthawks_deck.id},
 {question: "True or false?  Bullbat is another name for the Common Nighthawk.
-", answer: "true", deck_id: deck.id
+", answer: "true", deck_id: nighthawks_deck.id
 }]
 nighthawk.each do |card|
   Card.create(card)
