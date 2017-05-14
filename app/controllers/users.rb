c86cf6a4358ab9aebf065e6981ad29365724409e
Josh Lim & Ryan Austin <pair+joshhlim.ryana760@devbootcamp.com>
2017-05-13 18:33:09 -0700

    CSS style

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -35,8 +35,6 @@ end
 get '/users/:id' do
   erb :'users/login' unless session[:id]
   @user = User.find(params[:id])
-  @card = Card.find(params[:id])
-  @cards = Card.where(deck_id: @card.deck_id).count
   @rounds = Round.where(user_id: params[:id])
   erb :'users/profile'
 end
